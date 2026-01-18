04b68df581ff64105ce5275bb2e6aa6970f81969
taukir14 <tajammultaukir@gmail.com>
2026-01-18 17:17:03 +0300

    2nd commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+/* This is my first java program.
+* This will print 'Hello Everyone '
+*/
+public static void main(String [] args) {
+System.out.println("Hello Everyone");
+}
+}
