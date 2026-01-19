ad973adac7b9083cd9f8a992b35476d9b01f5b18
taukir14 <tajammultaukir@gmail.com>
2026-01-19 17:58:30 +0300

    5th commit

---
+++ JavaBasicCode/BasicOutput.java
@@ -0,0 +1,10 @@
+public class BasicOutput {    
+    public static void main(String[] args) {
+int d = 10;
+        System.out.println("1. println ");
+        System.out.println("2. println ");
+        System.out.print("1. print ");
+        System.out.printf("2. printf %d", d);
+    } 
+    
+}
