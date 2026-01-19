8ba9189318c40220fa8b937ff31c1b9d1e625d21
taukir14 <tajammultaukir@gmail.com>
2026-01-19 17:11:14 +0300

    4th commit

---
+++ JavaBasicCode/PrintVariables.java
@@ -0,0 +1,9 @@
+class PrintVariables {
+
+    public static void main(String[] args) {
+        double number = -10.6;
+
+        System.out.println("I am awesome.");
+        System.out.println("Number = " + number);
+    }
+}
