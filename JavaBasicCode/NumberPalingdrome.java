3db65ef112ffffbbd359569cb14a79af307023c9
taukir14 <tajammultaukir@gmail.com>
2026-01-22 16:20:32 +0300

    hhh

--- JavaBasicCode/NumberPalingdrome.java
+++ JavaBasicCode/NumberPalingdrome.java
@@ -5,16 +5,16 @@ public class NumberPalingdrome {
     
     public static void main(String[] args) {
         Scanner reff = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number :");
         int num = reff.nextInt();
         
         int sum  = 0;
-        while(num > 0){
+        while(0 < num){
             sum = sum * 10 + (num % 10);
 
             num /= 10;
         }
         System.out.println("The reverse of the number is: " + sum);
-        reff.close();
+        reff.close(); // Close the scanner
     }
 }
